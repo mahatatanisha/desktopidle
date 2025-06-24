6e8de2c8f959698b0681c185bfbf6c04c08e8fcf
mahatatanisha <95640901+mahatatanisha@users.noreply.github.com>
2025-06-25 02:41:03 +0530

    added local storage

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,29 @@
 const { ipcRenderer } = require('electron');
 
-function addTask() {
+window.onload = () => {
+  const savedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+  savedTasks.forEach(({ desc, time }) => {
+    addTask(desc, time);
+  });
+};
+
+function addTask(desc = '', time = '') {
   const taskContainer = document.getElementById('taskContainer');
   const div = document.createElement('div');
   div.classList.add('task');
   div.innerHTML = `
-    <input type="text" placeholder="Task" />
-    <input type="time" />
+    <input type="text" placeholder="Task" value="${desc}" />
+    <input type="time" value="${time}" />
+    <button class="delete-btn" title="Delete Task">🗑️</button>
   `;
   taskContainer.appendChild(div);
+
+  div.querySelector('.delete-btn').addEventListener('click', () => {
+    div.remove();
+    saveTasks(); // update storage after deletion
+  });
 }
+
 
 function saveTasks() {
   const tasks = [];
@@ -21,5 +35,22 @@ function saveTasks() {
     }
   });
 
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+    const box = document.getElementById('saved');
+  box.style.display = 'block';
+
+  setTimeout(() => {
+    box.style.display = 'none';
+  }, 6000);
+
+}
+
+
+function goToPet() {
+  
+const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+  tasks.forEach(({ desc, time }) => {
+    addTask(desc, time);
+  });
   ipcRenderer.send('switch-to-character', tasks);
 }
